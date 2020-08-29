c9d5aa1e4774fb4fae992664422d494592310de9
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-08-28 23:16:25 -0400

    This will print all the html

--- basic-concepts/content.js
+++ basic-concepts/content.js
@@ -1,5 +1,12 @@
-let contentText = document.getElementsByTagName('p');
-for (p in contentText){
-    console.log(contentText.textContent);
+let contentText = document.getElementsByTagName('body');
+console.log(contentText[0].innerHTML);
+// for (children in contentText){
+//     console.log(contentText.textContent);
     // this doesn't work for now
-}
+//}
+
+// const printChildren = (child) => {
+//     if (child.firstChild === null) {
+//         console.log(child.);
+//     }
+//}
