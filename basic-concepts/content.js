b856f8fd4670180d48a14e1da336953b0b597e0c
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-08-30 21:50:09 -0400

    This loads twice

--- basic-concepts/content.js
+++ basic-concepts/content.js
@@ -1,3 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-console.log(document.body);
- }, false);
+// document.addEventListener('DOMContentLoaded', () => {
+// console.log(document.body);
+//  }, false);
+
+ window.onload = () => {
+     let body = document.body;
+     let textContent = body.textContent || body.innerText;
+
+     console.log(textContent);
+     };
