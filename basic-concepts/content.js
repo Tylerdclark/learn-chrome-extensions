8aa972accc4a2a7406e6cbf0edc80b32ba1837f2
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-08-30 21:39:51 -0400

    cleaner but doesn't work in all cases.

--- basic-concepts/content.js
+++ basic-concepts/content.js
@@ -1,20 +1,3 @@
-const printChildren = (child) => {
-    if (child.firstChild === null) {
-        console.log(child);
-    } else{
-        for (grandBaby in child.childNodes){
-            printChildren(grandBaby)
-        }
-    }
-
-}
-
-let contentText = document.getElementsByTagName('body');
-//console.log(contentText[0].children);
-contentText[0].childNodes.forEach(n => {
-    printChildren(n)
-})
-// for (let i = 0; i < contentText.children.length; i++) {
-//     console.log(contentText.children[i].tagName);
-//   }
-
+document.addEventListener('DOMContentLoaded', () => {
+console.log(document.body);
+ }, false);
